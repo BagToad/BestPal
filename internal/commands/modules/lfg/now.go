c58f96f5230c14373bbc982c9b4640b242d5f44a
pengueeni <cbertrand620@gmail.com>
2025-10-19 17:29:50 -0400

    Updated error message of lfg now when not run in a game forum

--- internal/commands/modules/lfg/now.go
+++ internal/commands/modules/lfg/now.go
@@ -22,7 +22,8 @@ func (m *LfgModule) handleLFGNow(s *discordgo.Session, i *discordgo.InteractionC
 	// Must be invoked in a thread whose parent is the LFG forum
 	ch, err := s.Channel(i.ChannelID)
 	if err != nil || ch == nil || ch.ParentID != forumID {
-		_, _ = s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{Content: utils.StringPtr("❌ This command must be used inside an LFG thread.")})
+		forumChannelID := m.config.GetGamerPalsIntroductionsForumChannelID()
+		_, _ = s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{Content: utils.StringPtr(fmt.Sprintf("❌ This command must be used inside the desired games forum in <#%s>.", forumChannelID))})
 		return
 	}
 

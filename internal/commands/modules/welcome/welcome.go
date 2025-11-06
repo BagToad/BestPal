a099054eb9a28639c8284db2384844c3687f5219
Michael De Luca <92281368+mkael1@users.noreply.github.com>
2025-11-06 18:55:49 -0500

    Update welcome.go

--- internal/commands/modules/welcome/welcome.go
+++ internal/commands/modules/welcome/welcome.go
@@ -38,7 +38,6 @@ func (m *WelcomeModule) Register(cmds map[string]*types.Command, deps *types.Dep
 				discordgo.EnglishGB: "Set as automatic welcome message",
 			},
 		},
-		
 		HandlerFunc: m.handleSetWelcomeMsg,
 	}
 }
